762cda86374fafd01e5530a8771360857dd6ee73
JorgeRZT <georgeforceone@gmail.com>
2017-08-07 10:05:18 +0200

    Agregar archivos de proyecto.

---
+++ FluentInterface/Foo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentInterface
+{
+    public class Foo
+    {
+        public Foo Select()
+        {
+            return this;
+        }
+
+        public Foo Where()
+        {
+            return this;
+        }
+        public Foo OrderBy()
+        {
+            return this;
+        }
+
+    }
+}
---
+++ FluentInterface/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FluentInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new Foo();
+            foo.Select().Where().OrderBy().Select();
+        }
+    }
+}
